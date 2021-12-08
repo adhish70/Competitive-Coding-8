224d68cbf18999bffe33762e36cd3417eae5665c
Adhish Shrivastava <adhish70@gmail.com>
2021-12-08 13:26:45 -0500

    Initial Commit

---
+++ Problem-108 Flatten Binary Tree to Linked List.py
@@ -0,0 +1,17 @@
+# 114. Flatten Binary Tree to Linked List
+# https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:    
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        pass
---
+++ Problem-109 Minimum Window Substring.py
@@ -0,0 +1,11 @@
+# 76. Minimum Window Substring
+# https://leetcode.com/problems/minimum-window-substring/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        pass
